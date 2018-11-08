151844664c8ad7e1b9a5c3f592518aa301e409ec
Med <mehdi.gacem@versusmind.eu>
2018-11-08 11:15:09 +0100

    Inclusion of the HttpClientModule to us the HttpClient class for webservice consumption + Inclusion of CarsModule + Providing the the API_BASE_CONFIG to be injected in the CarService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CarsModule } from './cars/cars.module';
+import { CarsComponent } from './cars/cars.component';
+import { API_BASE_CONFIG } from './app.config';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { CarUpdateComponent } from './car-update/car-update.component';
+//import { ReactiveFormsModule } from '@angular/forms';
+//import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // CarUpdateComponent, 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CarsModule,
+    HttpClientModule,
+    //AppRoutingModule
+    //ReactiveFormsModule
   ],
-  providers: [],
+  providers: [{ provide : API_BASE_CONFIG, useValue : API_BASE_CONFIG.API_BASE_URL}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
